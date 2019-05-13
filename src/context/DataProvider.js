0f9ee4ac0c6002734f5cfeee82e5c3fe746d7116
Nicole Cedano <cedano.nicolee@gmail.com>
2019-05-12 22:11:00 -0600

    skeleton for provider

---
+++ src/context/DataProvider.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+const DataContext = React.createContext()
+
+class DataProvider extends Component{
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <DataContext.Provider value={{}}>
+                {this.props.children}
+
+            </DataContext.Provider>
+        )
+    }
+}
+
+export const withData = C => props => (
+    <ParkingContext.Consumer>
+        {value => <C {...props} {...value}/>}
+    </ParkingContext.Consumer>
+)
+
+export default DataProvider
