c071e62acddfc2e9a552756e8dcb5180d3c162f7
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-23 12:33:15 -0600

    mergin

---
+++ src/component/crime/Crime.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Crime extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Crime</h1>
+            </div>
+        );
+    }
+}
+
+export default Crime;
